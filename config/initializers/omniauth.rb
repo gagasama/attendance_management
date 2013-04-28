22c24a8ebd7ac010b18449f8cb03e403fe10f1a1
Makoto Nakayama <makoto0306@gmail.com>
2013-04-29 00:05:08 +0900

    add production google api key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,  
-     "139636662301-cmm6rdj3bg5rmqgkbgectuuueva4sbbb.apps.googleusercontent.com",
-     "cvmZew-O5U5yOxHHywv3BY3T",
-     { name: "google_login", approval_prompt: "" }
+  if Rails.env.production?
+    provider :google_oauth2,  
+      "139636662301-3m61n5v8f06teis4gnlvdgdmrfn70nuv.apps.googleusercontent.com",
+      "hkim5qH4lUMJPvqyM5YKt3jV",
+      { name: "google_login", approval_prompt: "" }
+  else  
+    provider :google_oauth2,  
+      "139636662301-cmm6rdj3bg5rmqgkbgectuuueva4sbbb.apps.googleusercontent.com",
+      "cvmZew-O5U5yOxHHywv3BY3T",
+      { name: "google_login", approval_prompt: "" }
+  end
 end
