78998710e77dfa36b4c52426259866a7884e59fe
Makoto Nakayama <makoto0306@gmail.com>
2013-04-30 22:46:48 +0900

    configure to use postgresql

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     uid "114534572318423517627"
     name "中山真琴"
     email "makoto0306@gmail.com"
-    refresh_token : "1/K4fBi1ITKYU3uxw3TaHIDLhiLoGXvXrjwbpkGFRRXRA"
+    refresh_token  "1/K4fBi1ITKYU3uxw3TaHIDLhiLoGXvXrjwbpkGFRRXRA"
     access_token "ya29.AHES6ZT8kzENbhWwkt1jidp_WkEzSO0MiNjN7R66cA1Ufh..."
     expires "1367174333"
   end
