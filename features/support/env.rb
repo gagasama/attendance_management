22308d68c5455cd9f26416579c331b3376902cc3
Makoto Nakayama <makoto0306@gmail.com>
2013-05-02 23:54:25 +0900

    adopt truncation strategy to delete data by every testing for selenium setting

--- features/support/env.rb
+++ features/support/env.rb
@@ -37,11 +37,12 @@ Spork.each_run do
   # recommended as it will mask a lot of errors for you!
   #
   ActionController::Base.allow_rescue = false
-  
+  Rails.logger.debug "ENV['SELENIUM']: #{ENV.inspect}" 
   # Remove/comment out the lines below if your app doesn't have a database.
   # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
   begin
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation #for selenium
+    #DatabaseCleaner.strategy = :transaction
   rescue NameError
     raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
   end
